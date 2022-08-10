401689bdcd8d154c366c695da972936f1bba12c3
Ubuntu20_J <761369177@qq.com>
2022-08-10 11:38:37 +0800

    2022-8-10

---
+++ base/string.py
@@ -0,0 +1,11 @@
+# name = "aBDA lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+fisrt_name = "mr"
+last_name = "python"
+full_name = fisrt_name + " " + last_name
+
+#print(full_name)
+print("Hello, " + full_name.title() + "!")
