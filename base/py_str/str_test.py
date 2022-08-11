9a16c72a15d066e7cb6e3c04c246f6b414a58dd8
Ubuntu20_J <761369177@qq.com>
2022-08-11 09:41:06 +0800

    2022-8-11

---
+++ base/py_str/str_test.py
@@ -0,0 +1,13 @@
+# name = "Hello Eric,would you like to learn some Python today?\n"
+# print(name)
+
+# name_1 = "Mr he!"
+# print("首字母大写展示" + name_1.title())
+# print("全字母大写展示" + name_1.upper())
+# print("全字母小写展示" + name_1.lower())
+
+name_2 = ' \n\tKoker ' 
+print(name_2)
+print(name_2.rstrip())
+print(name_2.lstrip())
+# print(name_2.strip())
